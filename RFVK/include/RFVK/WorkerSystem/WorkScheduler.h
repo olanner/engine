f520b8b17cd3be6c28ae299001ea09cc1d1a51c2
Olle Lannér <olle.23@hotmail.com>
2023-09-21 14:41:02 +0200

    triplebuffer changes

--- RFVK/include/RFVK/WorkerSystem/WorkScheduler.h
+++ RFVK/include/RFVK/WorkerSystem/WorkScheduler.h
@@ -8,36 +8,41 @@ class WorkScheduler
 {
 public:
 	typedef neat::static_vector<WorkType, WorkMax> AssembledWorkType;
-	
-	void BeginPush(neat::ThreadID threadID)
+
+	void	BeginPush(neat::ThreadID threadID)
 	{
 		std::scoped_lock lock(mySwapMutex);
-		mySchedules[int(threadID)].WriteNext();
+		mySchedules[int(threadID)].BeginNextWrite();
 	}
-	void PushWork(neat::ThreadID threadID, const WorkType& cmd)
+	void	PushWork(neat::ThreadID threadID, const WorkType& cmd)
 	{
-		mySchedules[int(threadID)].Push(cmd);
+		mySchedules[int(threadID)].Write().emplace_back(cmd);
 	}
-	void EndPush(neat::ThreadID threadID) {}
-	[[nodiscard]] AssembledWorkType& AssembleScheduledWork()
+	void	EndPush(neat::ThreadID threadID) {}
+
+	[[nodiscard]] AssembledWorkType& 
+			AssembleScheduledWork()
 	{
 		std::scoped_lock lock(mySwapMutex);
 			
 		myAssembledWork.clear();
 		for (const auto& scheduleID : myRegisteredSchedules)
 		{
-			for (const auto& cmd : mySchedules[int(scheduleID)].ReadNext())
+			mySchedules[int(scheduleID)].BeginNextRead();
+			for (const auto& cmd : mySchedules[int(scheduleID)].Read())
 			{
 				myAssembledWork.emplace_back(cmd);
 			}
 		}
 		return myAssembledWork;
 	}
-	void AddSchedule(neat::ThreadID threadID)
+
+	void	AddSchedule(neat::ThreadID threadID)
 	{
 		assert(int(threadID) < mySchedules.max_size() && int(threadID) >= 0 && "only 8 schedules allowed also no negative values >:(");
 		std::scoped_lock lock(mySwapMutex);
 		myRegisteredSchedules.emplace_back(threadID);
+		mySchedules[int(threadID)].AddFlags(neat::TripleBufferFlags::HasWrittenGuarantee);
 	}
 	
 private:
--- neat/Include/neat/Misc/TripleBuffer.h
+++ neat/Include/neat/Misc/TripleBuffer.h
@@ -3,52 +3,93 @@
 
 namespace neat
 {
+	enum class TripleBufferFlags : int
+	{
+		HasWrittenGuarantee	= 0x01,
+		HasReadGuarantee	= 0x02,
+	};
+	constexpr TripleBufferFlags operator|(TripleBufferFlags lhs, TripleBufferFlags rhs)
+	{
+		return static_cast<TripleBufferFlags>(
+			static_cast<std::underlying_type_t<TripleBufferFlags>>(lhs) |
+			static_cast<std::underlying_type_t<TripleBufferFlags>>(rhs));
+	}
+	constexpr bool operator&(TripleBufferFlags lhs, TripleBufferFlags rhs)
+	{
+		return
+			static_cast<std::underlying_type_t<TripleBufferFlags>>(lhs) &
+			static_cast<std::underlying_type_t<TripleBufferFlags>>(rhs);
+	}
+
 	template<typename T, unsigned MaxBuffSize>
 	class TripleBuffer
 	{
 	public:
-		const static_vector<T, MaxBuffSize>& ReadNext();
-		void WriteNext();
-		void Push(T&& anItem);
-		void Push(const T& anItem);
+		void	AddFlags(TripleBufferFlags flags);
+		void	BeginNextRead();
+		void	BeginNextWrite();
+		const static_vector<T, MaxBuffSize>&
+				Read();
+		static_vector<T, MaxBuffSize>&
+				Write();
 
 	private:
-		std::array<static_vector<T, MaxBuffSize>, 3> myBuffers;
-		unsigned myReadIndex = 0;
-		unsigned myWriteIndex = 1;
-		unsigned myFreeIndex = 2;
-		bool myIsNextAvailable = true;
+		TripleBufferFlags		myFlags = TripleBufferFlags(0);
+		std::array<static_vector<T, MaxBuffSize>, 3>
+								myBuffers;
+		unsigned				myReadIndex = 0;
+		unsigned				myWriteIndex = 1;
+		unsigned				myFreeIndex = 2;
+		bool					myHasWritten = true;
+		bool					myHasRead = true;
+		std::mutex				myMutex;
 	};
-	
+
 	template<typename T, unsigned MaxBuffSize>
-	inline const static_vector<T, MaxBuffSize>& TripleBuffer<T, MaxBuffSize>::ReadNext()
+	inline void TripleBuffer<T, MaxBuffSize>::AddFlags(TripleBufferFlags flags)
 	{
-		if (myIsNextAvailable)
+		myFlags = myFlags | flags;
+	}
+
+	template<typename T, unsigned MaxBuffSize>
+	inline void TripleBuffer<T, MaxBuffSize>::BeginNextRead()
+	{
+		if (!myHasWritten
+			&& myFlags & TripleBufferFlags::HasWrittenGuarantee)
 		{
-			std::swap(myReadIndex, myFreeIndex);
-			myIsNextAvailable = false;
+			return;
 		}
+		std::lock_guard lock(myMutex);
+		std::swap(myReadIndex, myFreeIndex);
+		myHasRead = true;
+		myHasWritten = false;
+	}
+
+	template<typename T, unsigned MaxBuffSize>
+	inline void TripleBuffer<T, MaxBuffSize>::BeginNextWrite()
+	{
+		if (myHasRead
+			&& myFlags & TripleBufferFlags::HasReadGuarantee)
+		{
+			return;
+		}
+		std::lock_guard lock(myMutex);
+		std::swap(myWriteIndex, myFreeIndex);
+		myBuffers[myWriteIndex].clear();
+		myHasRead = false;
+		myHasWritten = true;
+	}
+
+	template<typename T, unsigned MaxBuffSize>
+	inline const static_vector<T, MaxBuffSize>& TripleBuffer<T, MaxBuffSize>::Read()
+	{
 		return myBuffers[myReadIndex];
 	}
 
 	template<typename T, unsigned MaxBuffSize>
-	inline void TripleBuffer<T, MaxBuffSize>::WriteNext()
+	inline static_vector<T, MaxBuffSize>& TripleBuffer<T, MaxBuffSize>::Write()
 	{
-		std::swap(myWriteIndex, myFreeIndex);
-		myBuffers[myWriteIndex].clear();
-		myIsNextAvailable = true;
-	}
-
-	template<typename T, unsigned MaxBuffSize>
-	inline void TripleBuffer<T, MaxBuffSize>::Push(T&& anItem)
-	{
-		myBuffers[myWriteIndex].emplace_back(std::forward<T>(anItem));
-	}
-
-	template<typename T, unsigned MaxBuffSize>
-	inline void TripleBuffer<T, MaxBuffSize>::Push(const T& anItem)
-	{
-		Push(T(anItem));
+		return myBuffers[myWriteIndex];
 	}
 
 }
